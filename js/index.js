7b0dafc65b334f4727f2f429b4b9c199f7ff1f88
Lil-boss <rshaon09@gmail.com>
2022-09-28 12:48:51 +0600

    hero complete

--- js/index.js
+++ js/index.js
@@ -1,12 +1,14 @@
+let slideIndex = 0;
 function slider() {
   let i;
-  let slideIndex = 0;
-  let slides = document.getElementsByClassName("mySlides");
+
+  let slides = document.getElementsByClassName("slider");
   let dots = document.getElementsByClassName("dot");
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
   }
   slideIndex++;
+
   if (slideIndex > slides.length) {
     slideIndex = 1;
   }
@@ -15,5 +17,7 @@ function slider() {
   }
   slides[slideIndex - 1].style.display = "block";
   dots[slideIndex - 1].className += " active";
-  setTimeout(showSlides, 2000);
+  setTimeout(slider, 2000);
 }
+
+slider();
